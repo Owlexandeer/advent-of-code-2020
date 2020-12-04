0dd58679ceb6accb47995ca829a9a5409a3a2605
Alexander <alexander.s.webster@gmail.com>
2020-12-03 20:05:09 -0500

    Adds project scaffolding.

---
+++ filereader.js
@@ -0,0 +1,14 @@
+exports.readFile = (separator = "\n", toNumber = true) => {
+  return require("fs")
+    .readFileSync("input", "utf-8")
+    .split(separator)
+    .filter(Boolean)
+    .map(n => (toNumber ? parseInt(n) : n));
+};
+
+exports.readMovements = (separator = "\n", toNumber = true) => {
+  return require("fs")
+    .readFileSync("input", "utf-8")
+    .split(separator)
+    .filter(Boolean);
+};
